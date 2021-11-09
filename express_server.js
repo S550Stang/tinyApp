a67ec7c4dac1e0c2552c8556e8470ec1923a7c0a
S550Stang <prabhuvardhanrao94@gmail.com>
2021-11-09 21:29:13 +0000

    add views ejs

--- express_server.js
+++ express_server.js
@@ -7,9 +7,23 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-// this send you to the home page and its path is defaulted to /
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Welcome to HOMEPAGE GRINGO");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(urlDatabase[req.params.shortURL]);
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL] /* What goes here? */,
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
