79386a39fec681efc7f196fd8309c485e69c748e
S550Stang <prabhuvardhanrao94@gmail.com>
2021-11-10 00:45:52 +0000

    redirect to specified path

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
@@ -6,26 +7,42 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+function generateRandomString() {
+  ///generate random 6 digit string!
+  let r = Math.random().toString(20).substr(2, 6);
+  return r;
+}
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Welcome to HOMEPAGE GRINGO");
 });
-
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(`req.body = ${req.body.longURL}`);
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(`/urls/${randomString}`);
+  console.log(res.statusCode);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
   console.log(urlDatabase[req.params.shortURL]);
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL] /* What goes here? */,
+    longURL: urlDatabase[req.params.shortURL],
   };
   res.render("urls_show", templateVars);
 });
-
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.listen(PORT, () => {
   console.log(`TinyApp is listening on port: ${PORT}`);
 });
